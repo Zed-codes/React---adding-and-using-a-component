2f7813f1fa0a61369599cc22978734f29a16ac05
Zed-codes <mzineddine.b@hotmail.com>
2018-12-08 00:22:35 +0100

    N/A

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Hello from './component.js'
 
 class App extends Component {
   render() {
@@ -9,15 +10,15 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            <Hello name="Zineddine"/>
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://github.com/Zed-codes"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            My GitHub Account
           </a>
         </header>
       </div>
